957fd0eaaa4acbd4786c4cc8b1c3da5b2b9a7dd0
Nadeem Bitar <nadeem@gmail.com>
2023-09-22 05:42:24 -0700

    🎭 Refactor Hasql effect

--- src/Effectful/Hasql.hs
+++ src/Effectful/Hasql.hs
@@ -3,35 +3,30 @@ module Effectful.Hasql where
 import Effectful
 import Effectful.Dispatch.Dynamic
 import Effectful.Error.Static
-import Effectful.Reader.Static
 import Hasql.Pool (Pool, UsageError, use)
 import Hasql.Session (Session)
 
 -- | Provides the ability to execute actions against a postgresql database
 data Hasql :: Effect where
-  RunSession :: Session a -> Hasql m a
+  RunSession :: Error UsageError :> es => Session a -> Hasql (Eff es) a
 
 type instance DispatchOf Hasql = Dynamic
 
-runSession :: (Hasql :> es) => Session a -> Eff es a
+runSession :: (HasCallStack, Error UsageError :> es, Hasql :> es) => Session a -> Eff es a
 runSession = send . RunSession
 
 runHasqlIO ::
   forall es a.
-  ( IOE :> es,
-    Reader Pool :> es,
-    Error UsageError :> es
+  ( IOE :> es
   ) =>
+  Pool ->
   Eff (Hasql : es) a ->
   Eff es a
-runHasqlIO = interpret $ \_ -> \case
+runHasqlIO pool = interpret $ \env -> \case
   RunSession session -> do
-    pool <- ask @Pool
     r <- liftIO $ use pool session
-    either throwError pure r
+    localSeqUnlift env $ \unlift -> unlift $ do
+      either throwError pure r
 
-run :: Pool -> Eff [Hasql, Reader Pool, Error UsageError, IOE] a -> IO (Either (CallStack, UsageError) a)
-run pool = runEff . runError @UsageError . runReader pool . runHasqlIO
-
-runWithErrorHandler :: Pool -> (CallStack -> UsageError -> Eff '[IOE] a) -> Eff [Hasql, Reader Pool, Error UsageError, IOE] a -> IO a
-runWithErrorHandler pool errHandler = runEff . runErrorWith @UsageError errHandler . runReader pool . runHasqlIO
+run :: Pool -> Eff [Hasql, Error UsageError, IOE] a -> IO (Either (CallStack, UsageError) a)
+run pool = runEff . runError @UsageError . runHasqlIO pool
